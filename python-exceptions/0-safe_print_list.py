ac5158cc3cf1affc06432ae7818f0ad78abee64d
13865-commits <13865@holbertonstudents.com>
2026-04-29 17:51:07 +0000

    Safe print list with docs

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+"""Module that contains safe_print_list function"""
+
+
 def safe_print_list(my_list=[], x=0):
-    count = 0
+    """Prints x elements of a list and returns the real number printed"""
+    nb_printed = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            count += 1
+            nb_printed += 1
         except IndexError:
             break
     print("")
-    return (count)
+    return (nb_printed)
