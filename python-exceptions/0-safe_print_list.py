6ee770c01c7b8e0bb0b682a0c2011f3a17ed27e2
13865-commits <13865@holbertonstudents.com>
2026-04-29 17:56:33 +0000

    Initial commit for task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""Module that contains safe_print_list function"""
-
-
 def safe_print_list(my_list=[], x=0):
-    """Prints x elements of a list and returns the real number printed"""
-    nb_printed = 0
+    count = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            nb_printed += 1
+            count += 1
         except IndexError:
             break
     print("")
-    return (nb_printed)
+    return (count)
