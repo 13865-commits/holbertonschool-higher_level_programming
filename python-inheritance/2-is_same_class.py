9887627da59ed0ec7b2bef905acec876b60d3eaa
13865-commits <13865@holbertonstudents.com>
2026-05-27 09:14:11 +0000

    Fix pycodestylclass

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,14 +6,5 @@ an instance of a specified class.
 
 
 def is_same_class(obj, a_class):
-    """
-    Check if obj is exactly an instance of a_class.
-    
-    Args:
-        obj: The object to check.
-        a_class: The class to compare against.
-        
-    Returns:
-        True if obj is exactly an instance of a_class, otherwise False.
-    """
+    """Check if obj is exactly an instance of a_class."""
     return type(obj) is a_class
