bd67a2296bcd6b86904814cad3fef459fefb3dda
13865-commits <13865@holbertonstudents.com>
2026-05-28 19:11:34 +0000

    Fix pycodestym

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,10 +6,5 @@ of a class that inherited from the specified class.
 
 
 def inherits_from(obj, a_class):
-    """
-    Check if obj is an instance of a subclass of a_class.
-    
-    Returns True if obj is an instance of a class that inherited
-    (directly or indirectly) from a_class; otherwise False.
-    """
+    """Check if obj is an instance of a subclass of a_class."""
     return isinstance(obj, a_class) and type(obj) is not a_class
