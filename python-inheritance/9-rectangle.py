e49b82e91ecdbb21378890a31131b3ee42a72b85
13865-commits <13865@holbertonstudents.com>
2026-05-28 19:24:44 +0000

    9. Full rece

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class that inherits from BaseGeometry.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A representation of a rectangle."""
+
+    def __init__(self, width, height):
+        """Initialize a new Rectangle instance."""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the print/str representation of the rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
