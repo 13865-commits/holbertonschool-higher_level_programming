9e43ef7e4e5cb4bf4a68c8aca3d6b9f17885d341
13865-commits <13865@holbertonstudents.com>
2026-06-09 18:23:29 +0000

    Fix PEP8 line too long error in task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,7 +3,9 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string at the end of a text file (UTF8) and returns characters added.
+    """Appends a string at the end of a text file (UTF8)
+
+    and returns the number of characters added.
 
     Args:
         filename (str): The name of the file to append to.
