af4b62611f9d2a533bee084e76c7352ee0d2322a
13865-commits <13865@holbertonstudents.com>
2026-07-25 13:49:03 +0000

    2. Filter states by user input using format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     cur = db.cursor()
     query = (
         "SELECT * FROM states WHERE name = '{}' "
-        "ORDER BY states.id ASC".format(sys.argv[4])
-    )
+        "ORDER BY states.id ASC"
+    ).format(sys.argv[4])
     cur.execute(query)
     query_rows = cur.fetchall()
     for row in query_rows:
