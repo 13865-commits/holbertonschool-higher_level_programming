ed98a35792c07d731a6699d69b81f6f4f5c44d5e
13865-commits <13865@holbertonstudents.com>
2026-07-25 13:56:45 +0000

    3. SQL Injection safe filter states implementation

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+        "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
         (sys.argv[4],)
     )
     query_rows = cur.fetchall()
